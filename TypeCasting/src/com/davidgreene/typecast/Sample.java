2eff233cec64d5fe9088d14501023d19c0283d14
GreeneD <GreeneD@EDGE2STMT480008.bsg.ad.adp.com>
2021-03-20 11:07:12 -0400

    initial commit for typecasting lesson

---
+++ TypeCasting/src/com/davidgreene/typecast/Sample.java
@@ -0,0 +1,33 @@
+package com.davidgreene.typecast;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Implicit type Casting");
+		char a = 'A';
+		System.out.println("Value of a is " + a);
+		
+		int b=1024;
+		System.out.println("Value of b is " + b);
+		
+		float c = 12.59f;
+		System.out.println("Value of c is " + c);
+		
+		long d = b;
+		System.out.println("Value of d is " + d);
+		
+		double e = b;
+		System.out.println("Value of e is " + e);
+		
+		int ff = (int)c;
+		System.out.println("Value of ff is " + ff);
+	}
+	
+	
+	
+	public String display(int a, int b) {
+		return "done";
+	}
+
+}
---
+++ phase4_frontend/script.js
@@ -0,0 +1,9 @@
+console.log('hello3');// this is a comment
+
+/*
+console.log('hello3');
+console.log('hello3');
+console.log('hello3');
+console.log('hello3');
+*/
+console.log('hello3 ' + (5 + 5));
